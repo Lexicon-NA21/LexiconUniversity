d347ba54a91e52450a3e17f8f445e7516653a69b
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-03-18 12:59:48 +0100

    Add project files.

---
+++ LexiconUniversity/Models/Entities/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconUniversity.Models.Entities
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public int Grade { get; set; }
+
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+
+        //Navigation property
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+
+    }
+}
---
+++ LexiconUniversity/Models/Entities/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconUniversity.Models.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Avatar { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        //Navigation Property
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+
+
+       //Många till många
+       // public ICollection<Course> Courses { get; set; }
+
+    }
+}
